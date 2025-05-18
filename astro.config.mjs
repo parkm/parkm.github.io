87399f05a4083b036244e0b66b04851327fd8ad9
Parker Miller <parker.miller@canopytax.com>
2025-05-18 14:25:35 -0600

    Update deps

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,15 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
-import tailwind from '@astrojs/tailwind';
+import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://parkermiller.net',
-  integrations: [tailwind()],
   server: {
     host: true,
+  },
+  vite: {
+    plugins: [tailwindcss()]
   }
 });
