dbfe6225b52343b655185ed1f0136072b3107175
Parker Miller <parkermiller00@gmail.com>
2025-11-20 21:46:23 -0700

    Add select component

--- src/components/desktop-ui/DesktopUiDemo.tsx
+++ src/components/desktop-ui/DesktopUiDemo.tsx
@@ -8,25 +8,49 @@ import {
   MenubarShortcut,
   MenubarSeparator,
 } from "./Menubar";
+import { Select, SelectOption } from "./Select";
 
 export function DesktopUiDemo() {
   return (
     <div className="w-full h-screen flex flex-col">
-      <Menubar>
-        <MenubarMenu>
-          <MenubarTrigger>File</MenubarTrigger>
-          <MenubarContent>
-            <MenubarItem>
-              New Tab <MenubarShortcut>⌘T</MenubarShortcut>
-            </MenubarItem>
-            <MenubarItem>New Window</MenubarItem>
-            <MenubarSeparator />
-            <MenubarItem>Share</MenubarItem>
-            <MenubarSeparator />
-            <MenubarItem>Print</MenubarItem>
-          </MenubarContent>
-        </MenubarMenu>
-      </Menubar>
+      <div>
+        <Menubar className="flex-1 flex items-center gap-2 justify-between">
+          <div className="flex items-center gap-2">
+            <MenubarMenu>
+              <MenubarTrigger>File</MenubarTrigger>
+              <MenubarContent>
+                <MenubarItem>
+                  New Tab <MenubarShortcut>⌘T</MenubarShortcut>
+                </MenubarItem>
+                <MenubarItem>New Window</MenubarItem>
+                <MenubarSeparator />
+                <MenubarItem>Share</MenubarItem>
+                <MenubarSeparator />
+                <MenubarItem>Print</MenubarItem>
+              </MenubarContent>
+            </MenubarMenu>
+            <MenubarMenu>
+              <MenubarTrigger>Edit</MenubarTrigger>
+              <MenubarContent>
+                <MenubarItem>
+                  Cut <MenubarShortcut>⌘X</MenubarShortcut>
+                </MenubarItem>
+                <MenubarItem>
+                  Copy <MenubarShortcut>⌘C</MenubarShortcut>
+                </MenubarItem>
+                <MenubarItem>
+                  Paste <MenubarShortcut>⌘V</MenubarShortcut>
+                </MenubarItem>
+              </MenubarContent>
+            </MenubarMenu>
+          </div>
+          <Select className="min-w-60 justify-self-end">
+            <SelectOption value="1">Option 1</SelectOption>
+            <SelectOption value="2">Option 2</SelectOption>
+            <SelectOption value="3">Option 3</SelectOption>
+          </Select>
+        </Menubar>
+      </div>
       <div className="flex-1">
         <QuadDock
           top={<div>top</div>}
